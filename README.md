5198ed4ecad300713adf2075418bf747c4d3e56b
SuchPigeon <kurynovv@yandex.ru>
2026-04-19 01:30:45 +0500

    Go game and naive agent bot implementation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # simple_go_game_learning
+
+```python bot_v_bot.py``` to simulate random naive bots play
+
+
+## Learning Resources
+
+**Deep Learning and the Game of Go** by Kevin Ferguson & Max Pumperla (Manning, 2019)  
+Companion code/tutorials for building Go AI with Keras/TensorFlow.  
+- Book: https://www.manning.com/books/deep-learning-and-the-game-of-go  
+- GitHub: https://github.com/maxpumperla/deep_learning_and_the_game_of_go
---
+++ bot_v_bot.py
@@ -0,0 +1,25 @@
+from game import goboard
+from game import gotypes
+from game.agent import naive
+from game.utils import print_board, print_move
+import time
+
+def main():
+    board_size = 9
+    game = goboard.GameState.new_game(board_size)
+    bots = {
+        gotypes.Player.black: naive.RandomBot(),
+        gotypes.Player.white: naive.RandomBot(),
+    }
+
+    while not game.is_over():
+        time.sleep(0)
+
+        print(chr(27) + "[2j")
+        print_board(game.board)
+        bot_move = bots[game.next_player].select_move(game)
+        print_move(game.next_player, bot_move)
+        game = game.apply_move(bot_move)
+
+if __name__ == '__main__':
+    main()
